42744a7188d5aa1788b0917a42c5a0cdfc653175
Engin KUK <kuk.engin@gmail.com>
2020-07-16 02:44:45 +0300

    unit tests completed

--- iSearcherParseTests/iSearcherParseTests.swift
+++ iSearcherParseTests/iSearcherParseTests.swift
@@ -48,10 +48,50 @@ class iSearcherParseTests: XCTestCase {
  
     }
 
+    
+    func testDeleted() {
+          
+           let expectedCount = 95
+           let promise = expectation(description: "Status code: 200")
+        
+        
+           sut.performSearch(for: "Swift", completion: {success in
+                 if success {
+                   
+                   switch self.sut.state {
+               case .notSearchedYet, .loading, .noResults:
+                 return
+               case .results(let list):
+                  
+                deletedItems  = list.prefix(5).map{ $0.storeURL }
+                self.sut.filterDeleted()
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    let extractedCount = list.count
+                    XCTAssertEqual(extractedCount, expectedCount, "Failed to get number of expected search results after deleting 5 items")
+                      }
+                
+                deletedItems = []
+                UserDefaults.standard.set(deletedItems, forKey: "deletedItems")
+                // be aware line above will remove all persistent deleted items
+              }
+                    promise.fulfill()
+
+                  }
+              })
+        
+             
+            wait(for: [promise], timeout: 5)
+    
+       }
+    
+    
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         measure {
-            // Put the code you want to measure the time of here.
+            
+            sut.performSearch(for: "Adale", completion: {success in  })
+       
         }
     }
 
